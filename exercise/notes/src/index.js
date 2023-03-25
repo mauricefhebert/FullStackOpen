b879d2c4a5f08b4dc764b85fd0bfd4a21c0a6b1e
mauricefh <filiatreaultmaurice@gmail.com>
2023-03-25 17:17:20 -0400

    completed the learning section of part2-d

--- exercise/notes/src/index.js
+++ exercise/notes/src/index.js
@@ -1,5 +1,12 @@
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+axios.get("http://localhost:3001/notes").then(response => {
+  const notes = response.data;
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App notes={notes} />
+  );
+});
